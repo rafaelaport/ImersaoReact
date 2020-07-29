4f79f58a26da36b2c678fdb54ddf7777f323910a
Matheus - <rafaela.portugal96@gmail.com>
2020-07-29 19:29:04 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -12,7 +13,7 @@ function Menu() {
                 <img className="Logo" src={Logo} alt="Raflix logo" />
             </a>
 
-            <Button as='a' className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="cadastro/video">
                     Novo Vídeo
             </Button>
         </nav>
